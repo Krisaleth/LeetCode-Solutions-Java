767a89f6d7f47533875598d79ad7667f3dc9cd89
KimTam <kimtam486@gmail.com>
2026-01-14 21:35:42 +0700

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 48 MB (88.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) {
+        int left = findBound(nums, target, true);
+        if (left == -1) {
             return new int[] {-1, -1};
         }
-        int left = leftTarget(nums, target);
-        int right = rightTarget(nums, target);
+        int right = findBound(nums, target, false);
 
         return new int[] {left, right};
     }
 
-    public int leftTarget(int[] nums, int target) {
+    public int findBound(int[] nums, int target, boolean isFirst) {
         int left = 0;
         int right = nums.length - 1;
         int mid = 0;
@@ -19,7 +19,12 @@ class Solution {
             mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 ans = mid;
-                right = mid - 1;
+                if (isFirst) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
             }
 
             else if (nums[mid] < target) {
@@ -33,27 +38,4 @@ class Solution {
         return ans;
     }
 
-    public int rightTarget(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int mid = 0;
-        int ans = -1;
-
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                left = mid + 1;
-            }
-
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1; 
-            }
-        }
-
-        return ans;
-    }
 }
