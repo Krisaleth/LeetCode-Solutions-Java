f1a4f0842ecc69052f75d1d98a251c9236599def
KimTam <kimtam486@gmail.com>
2026-01-20 09:36:28 +0700

    0020-VALID-PARENTHESES Stats: Time: 5 ms (17.22%), Space: 43.9 MB (5.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,36 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stackS = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
-                stackS.push(c);
-            } else {
-                if (stackS.isEmpty()) return false;
-                char top = stackS.peek();
-                if ((c == ')' && top == '(') ||
-                    (c == ']' && top == '[') ||
-                    (c == '}' && top == '{')) {
-                        stackS.pop();
+        Stack<Character> check = new Stack<>();
+        int i = 0;
+        if (!s.contains(")") && !s.contains("}") && !s.contains("]")) {
+            return false;
+        }
+        while(i < s.length()) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                check.push(s.charAt(i));
+            }
+            else if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}') {
+                if (!check.empty()) {
+                    if (s.charAt(i) == ')' && check.peek() == '(' ||
+                    s.charAt(i) == ']' && check.peek() == '[' ||
+                    s.charAt(i) == '}' && check.peek() == '{') { 
+                        check.pop();
                     }
                     else {
                         return false;
                     }
+                }
+                else {
+                    return false;
+                }
             }
-            
+            i++;
         }
-        
-        return stackS.isEmpty();
+        if (check.empty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
