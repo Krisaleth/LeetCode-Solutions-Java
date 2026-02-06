ce64f77f3807690976bee022b80eb4dea8a19deb
KimTam <kimtam486@gmail.com>
2026-02-06 22:39:54 +0700

    0739-DAILY-TEMPERATURES Stats: Time: 79 ms (13.13%), Space: 108 MB (7.24%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int i = 1;
         int length = temperatures.length;
         int[] res = new int[length];
         Stack<Integer> tempStack = new Stack<>();
         tempStack.push(0);
-        while (i < length) {
+        for (int i = 1; i < length; i++) {
             while(!tempStack.isEmpty() && temperatures[tempStack.peek()] < temperatures[i]) {
                 res[tempStack.peek()] = i-tempStack.peek();
                 tempStack.pop();
             }
             tempStack.push(i);
-            i++;
         }
 
         return res;
